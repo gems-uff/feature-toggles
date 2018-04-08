81d7800509b793ff260b7d26bb4991fa19693a37
edusmil <eduardosmil@gmail.com>
2018-04-08 16:20:32 -0300

    Create git_stats_api.py
    commit de stats api

---
+++ git_stats_api.py
@@ -0,0 +1,100 @@
+from github import Github
+import  requests
+import mysql.connector
+from json2html import json2html
+from datetime import datetime
+from requests.auth import HTTPBasicAuth
+
+class CONST(object):
+    BD_USER = "bdd_dissertacao"
+    BD_PASSWORD = "Smil123!"
+    BD_HOST = "50.62.209.195"
+    BD_DATABASE = "uff_bdd_dissertacao"
+
+    def __setattr__(self, *_):
+        pass
+
+CONST = CONST()
+
+'''
+    headers = {'Accept': 'application/vnd.github.cloak-preview'}
+    url = "https://api.github.com/search/commits?q=repo:"+rep1+"+merge:true" 
+    data = requests.get(url,headers=headers).json()
+    print(json2html.convert(json=data,table_attributes='border="1"'))
+    '''
+    cnx = mysql.connector.connect(user=CONST.BD_USER, password=CONST.BD_PASSWORD,
+                              host=CONST.BD_HOST,
+                              database=CONST.BD_DATABASE,connection_timeout=300)
+    cursor = cnx.cursor(buffered=True)
+    select_repository = ("SELECT id, full_name FROM git_table t where not exists (select 1 from git_stats s where s.id_repo=t.id and git_last_commit is not null) order by id;")
+    cursor.execute(select_repository)
+    rs_git_control = cursor.fetchall()
+    i=0
+    for _id, _full_name in rs_git_control:
+        rpo_det = g.get_repo(_full_name)
+
+        commit =rpo_det.get_commits()
+        '''cont = rpo_det.get_contributors()
+        branches = rpo_det.get_branches()
+            
+        n_contributors = 0
+        n_commits = 0
+        n_branches = 0
+        
+        if cont is not None:
+            n_contributors = len(list(cont))'''
+
+        if commit is not None:
+            n_commits = len(list(commit))
+            sha = commit[0].sha
+            _senha = "Basic ZWR1c21pbDpTbWkxMjMh"
+            headers = {'Accept': 'application/vnd.github.cloak-preview'}
+            url = "https://api.github.com/repos/"+_full_name+"/commits/"+sha
+            data = requests.get(url,headers=headers).json()
+            try:
+                dt_last_commit = datetime.strptime(data["commit"]["committer"]["date"] ,'%Y-%m-%dT%H:%M:%SZ')
+            except KeyError:
+                dt_last_commit = None
+        '''    
+        if branches is not None:
+            n_branches = len(list(branches))'''
+
+        ''' 
+        headers = {'Accept': 'application/vnd.github.cloak-preview'}
+        url = "https://api.github.com/search/commits?q=repo:"+_full_name+"+merge:true&rel=last"
+        data = requests.get(url,headers=headers).json()
+        n_merges = data.get("total_count","{date}")'''
+
+        
+        cnx = mysql.connector.connect(user=CONST.BD_USER, password=CONST.BD_PASSWORD,
+                              host=CONST.BD_HOST,
+                              database=CONST.BD_DATABASE,connection_timeout=300)
+        cursor = cnx.cursor(buffered=True) 
+
+        select_repository = ("SELECT 1 FROM git_stats where id_repo=" + str(_id) +";")
+        cursor.execute(select_repository)
+        if cursor.rowcount == 0:
+            add_statistics = ("INSERT INTO git_stats (id_repo, git_commits, git_branches, git_merges, git_contributors, git_last_commit) "
+                                " values (" + str(_id) + "," + str(n_commits) +"," + str(n_branches) + ","+str(n_merges)+"," + str(n_contributors)+",")
+            if dt_last_commit is not None:
+                add_statistics = add_statistics + ("(STR_TO_DATE('"+dt_last_commit.strftime("%d-%m-%Y")+"', '%d-%m-%Y'))); ")
+            else:
+                add_statistics = add_statistics + ("null); ")
+            cursor.execute(add_statistics) 
+        else:
+            if dt_last_commit is not None:
+                add_statistics = ("update git_stats set git_last_commit=(STR_TO_DATE('"+dt_last_commit.strftime("%d-%m-%Y")+"', '%d-%m-%Y')) where id_repo=" + str(_id) + ";")
+                cursor.execute(add_statistics) 
+        cnx.commit()    
+
+        cnx.close()  
+        '''
+        else:
+            update_statistics = ("UPDATE git_stats " +
+                                " set  git_commits=" + str(n_commits) +"," +
+                                "      git_branches=" + str(n_branches) +"," + 
+                                "      git_merges="+str(n_merges)+"," + 
+                                "      git_contributors=" + str(n_contributors) +  
+                                " where id_repo=" + str(_id) + ";")
+        cursor.execute(update_statistics) 
+        '''
