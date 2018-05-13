e3096a8ce67ce9beec68e903d75628e0593c735d
Eduardo <eduardosmil@gmail.com>
2018-05-13 18:09:27 -0300

    git checkout rev1

--- git_checkout.py
+++ git_checkout.py
@@ -136,10 +136,43 @@ for row in cursor._rows:
 
         _split_commit = lista_commit.split("\n")
         sql_insert = ""
+        sql_insert_merge_branch = ""
         
         for _commit in _split_commit:
             if _commit != "":
                 sql_insert = sql_insert + "(" + str(row[0].decode("utf-8")) +  ",5,now(),'" + str(_commit) + "',NULL),"
+
+                print("Verificando Merge Parents...")
+                lista_commit_parents = getParentCommitMerge(_commit)
+                if lista_commit_parents != "":
+                    _split_commit_parents = lista_commit_parents.split(" ")
+                    git_parent = getMergeBase(_split_commit_parents(0),_split_commit_parents(1))
+
+                    _mais_dev_1 = False
+                    _mais_dev_2 = False
+
+                    if git_parent != "":
+                        _lista_commit_b1 = getCommitsBetween(git_parent,_split_commit_parents(0))
+                        _lista_commit_b2 = getCommitsBetween(git_parent,_split_commit_parents(2))
+
+                        print("Merge: " + _commit + " Pai 1:" + _split_commit_parents(0))
+
+                        _split_commit_b1 = _lista_commit_b1.split("\n")
+                        _lista_commit_p1 = getAuthorsBetween(git_parent,_split_commit_b1(0))
+                        
+                        _split_commit_p1 = _lista_commit_p1.split("\n")
+                        if len(_split_commit_p1) >= 2: # mais de 2 desenvolvedores
+                            _mais_dev_1 = True
+                        
+                        _lista_commit_p2 = getAuthorsBetween(git_parent,_lista_commit_b2(0))
+                        
+                        _split_commit_p2 = _lista_commit_p2.split("\n")
+                        if len(_lista_commit_p2) >= 2: # mais de 2 desenvolvedores
+                            _mais_dev_2 = True
+
+                        if (_mais_dev_1 = True) and (_mais_dev_2 = True):
+                            print("Merge Branch: " + str(_commit))
+                            sql_insert_merge_branch = sql_insert_merge_branch + + "(" + str(row[0].decode("utf-8")) +  ",6,now(),'" + str(_commit) + "',NULL),"
             
         cnx = mysql.connector.connect(user=CONST.BD_USER, password=CONST.BD_PASSWORD,
                             host=CONST.BD_HOST,
@@ -150,6 +183,33 @@ for row in cursor._rows:
        	    insert_search= "insert into git_stats_local (id_repo, id_stats, timestamp, stats_value, stats_value_aux) values " + sql_insert + ";"
             cursor.execute(insert_search)
             cnx.close()
+
+        if sql_insert_merge_branch != "":
+            sql_insert_merge_branch = sql_insert_merge_branch[:len(sql_insert_merge_branch)-1]
+       	    insert_search= "insert into git_stats_local (id_repo, id_stats, timestamp, stats_value, stats_value_aux) values " + sql_insert_merge_branch + ";"
+            cursor.execute(insert_search)
+            cnx.close()
+
+        lista_commit = subprocess.check_output(["git log --regexp-ignore-case --grep 'merge branch' --format=format:%H "],stderr=subprocess.STDOUT,shell=True)
+        lista_commit = lista_commit.decode("utf-8")
+
+        _split_commit = lista_commit.split("\n")
+        sql_insert = ""
+        
+        for _commit in _split_commit:
+            if _commit != "":
+               sql_insert = sql_insert + "(" + str(row[0].decode("utf-8")) +  ",7,now(),'" + str(_commit) + "',NULL),"
+            
+        cnx = mysql.connector.connect(user=CONST.BD_USER, password=CONST.BD_PASSWORD,
+                            host=CONST.BD_HOST,
+                            database=CONST.BD_DATABASE,connection_timeout=300,buffered=True)
+        cursor = cnx.cursor()
+        if sql_insert != "":
+            sql_insert = sql_insert[:len(sql_insert)-1]
+       	    insert_search= "insert into git_stats_local (id_repo, id_stats, timestamp, stats_value, stats_value_aux) values " + sql_insert + ";"
+            cursor.execute(insert_search)
+            cnx.close()
+
  
     except subprocess.CalledProcessError as e:
         print(e.output)
