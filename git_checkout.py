3f482b5f282d69bff03eacbdeb8e3fbc4d1b9b8a
Eduardo <eduardosmil@gmail.com>
2018-05-13 20:07:38 -0300

    git checkout rev2

--- git_checkout.py
+++ git_checkout.py
@@ -101,8 +101,8 @@ for row in cursor._rows:
                     ultimo_commit = str(_commit) 
                
                 print(str(_commit))
-                subprocess.check_output(["git checkout -f "+_commit],stderr=subprocess.STDOUT,shell=True)
-                git_grep =grep(str(row[3].decode("utf-8")),str(row[4].decode("utf-8")))
+                #subprocess.check_output(["git checkout -f "+_commit],stderr=subprocess.STDOUT,shell=True)
+                #git_grep =grep(str(row[3].decode("utf-8")),str(row[4].decode("utf-8")))
 
                 flag_fw = ""
            
