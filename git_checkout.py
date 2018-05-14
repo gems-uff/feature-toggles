924261b7855e99650d9b33a8ca4beeaf32f8df89
Eduardo <eduardosmil@gmail.com>
2018-05-13 21:52:23 -0300

    git checkout rev 8

--- git_checkout.py
+++ git_checkout.py
@@ -160,23 +160,23 @@ for row in cursor._rows:
                         print("Merge: " + _commit + " Pai 1:" + _split_commit_parents[0])
 
                         _split_commit_b1 = _lista_commit_b1.split("\n")
-                        _lista_commit_p1 = getAuthorsBetween(git_parent,_split_commit_b1[0])
+                        _lista_dev_p1 = getAuthorsBetween(git_parent,_split_commit_b1[0])
                         
-                        _split_commit_p1 = _lista_commit_p1.split("\n")
-                        print("Desenvolvedores 1:" + str(len(_split_commit_p1)))
-                        if len(_split_commit_p1) >= 2: # mais de 2 desenvolvedores                            
+                        _split_dev_p1 = _lista_dev_p1.split("\n")
+                        print("Desenvolvedores 1:" + git_parent + " " + _split_commit_b1[0] + " " + _lista_dev_p1)
+                        if len(_split_dev_p1) >= 2: # mais de 2 desenvolvedores                            
                             _mais_dev_1 = True
                         
                         print("Merge: " + _commit + " Pai 2:" + _split_commit_parents[1])
 
                         _split_commit_b2 = _lista_commit_b2.split("\n")
-                        _lista_commit_p2 = getAuthorsBetween(git_parent,_split_commit_b2[0])
+                        _lista_dev_p2 = getAuthorsBetween(git_parent,_split_commit_b2[0])
                         
-                        _split_commit_p2 = _lista_commit_p2.split("\n")
-                        print("Desenvolvedores 2:" + str(len(_split_commit_p2)))
-                        if len(_split_commit_p2) >= 2: # mais de 2 desenvolvedores                            
+                        _split_dev_p2 = _lista_dev_p2.split("\n")
+                        print("Desenvolvedores 2:" + git_parent + " " + _split_commit_b2[0] + " " + _lista_dev_p2)
+                        if len(_split_dev_p2) >= 2: # mais de 2 desenvolvedores                            
                             _mais_dev_2 = True
-
+                        exit()
                         if ((_mais_dev_1 == True) and (_mais_dev_2 == True)):
                             print("Merge Branch..............: " + str(_commit))
                             sql_insert_merge_branch = sql_insert_merge_branch + "(" + str(row[0].decode("utf-8")) +  ",6,now(),'" + str(_commit) + "',NULL),"
