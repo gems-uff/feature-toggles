d79e047f37530c6ce9f34d8695b5ebd76b097f35
Eduardo <eduardosmil@gmail.com>
2018-05-17 08:56:13 -0300

    issues

--- git_issues.py
+++ git_issues.py
@@ -1,1 +1,18 @@
-teste1
+import mysql.connector
+from github import Github
+
+
+# using username and password
+#g = Github("user", "password")
+
+# or using an access token
+g = Github("U-k9OBXUjX0AvtRoZIpAjg")
+
+
+r = g.get_repo("edusmil/feature_toggles")
+print(r)
+issues = r.get_issues()
+print(issues)
+
+
+
